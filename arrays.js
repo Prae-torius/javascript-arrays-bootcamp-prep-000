b34627cb4a64551fd1db04c4fea7eb1a647ab729
Preston A Arnette <arnettepreston@gmail.com>
2018-08-08 14:59:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snikers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   var array = []
-  array[1] = "foo"
+  array[0] = "foo"
   }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
